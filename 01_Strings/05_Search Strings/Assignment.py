f1cb034f90f226df4775ae7be5f549a548c031ab
Jose Jimenez <Jimenjos000@warren.k12.in.us>
2024-01-23 12:02:12 -0500

    dfdfg

--- 01_Strings/05_Search Strings/Assignment.py
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -1,7 +1,7 @@
 text=input("Enter some text: ")
 index = text.find("the") != 1
 if index !=-1:
-    print("This sentence contains the word 'the'")
+    # print("This sentence contains the word 'the'")
     print("It is located at index", index)
 else:
     print("This sentence does not contain the word 'the'")
