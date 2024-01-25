1a87932cac688ff1ed17f3280f4d707efb9cde20
Jose Jimenez <Jimenjos000@warren.k12.in.us>
2024-01-25 12:01:23 -0500

    fgfh

--- 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
+++ 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
@@ -1,7 +1,11 @@
+# try function Checks code for errors
 try:
     First_value = int(input('Enter a number: '))
     Second_vaule=int(input("Enter another : "))
     print('The answer is: ', Second_vaule/First_value)
+
+# except function lets you fix the error
+# else block lets you execute code when there is no error
 
 except ZeroDivisionError:
     print("You provided a 0 and division by 0 is not possible, sorry")
