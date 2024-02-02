963fcd9c2bb55a31f41788f0d2101c0a579d57fb
Jose Jimenez <Jimenjos000@warren.k12.in.us>
2024-02-02 12:02:01 -0500

    sdfwer67u

---
+++ bored.py
@@ -0,0 +1,13 @@
+# COST_OF_FRISBEE=15
+# Frisbees=3
+
+# total_cost=COST_OF_FRISBEE*Frisbees
+
+# print("The total cost is $", total_cost, sep="")
+
+COST_OF_FRISBEE=15
+Frisbees= input("How many frisbees wuold you like")
+
+total_cost=COST_OF_FRISBEE*Frisbees
+
+print("The total cost is $", total_cost, sep="")
