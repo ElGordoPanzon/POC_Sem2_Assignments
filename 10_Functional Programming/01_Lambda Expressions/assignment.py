5d93699b278c40376ba900e50047722d753463f9
Jose Jimenez <Jimenjos000@warren.k12.in.us>
2024-03-20 11:44:47 -0400

    tttt

---
+++ 10_Functional Programming/01_Lambda Expressions/assignment.py
@@ -0,0 +1,30 @@
+
+def product (x,y):
+    return x*y
+
+lambda x, y : x * y
+
+print(product(15,13))
+
+def quotient (x, y):
+    return x/y
+
+lambda x, y : x /y
+
+print(quotient(15, 3))
+
+def exponent (x, y):
+    return x**y
+
+lambda x, y:x**y
+
+print(exponent(3, 7))
+
+
+product = lambda a, b: a * b
+quotient = lambda a, b: a / b
+exponent = lambda a, b: a ** b
+
+print(product(15, 13))
+print(quotient(15, 13))
+print(exponent(3, 7))
