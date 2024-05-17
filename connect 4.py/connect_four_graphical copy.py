7bb016c6e718443ea794e1991fb3241df9010b2b
Jose Jimenez <Jimenjos000@warren.k12.in.us>
2024-05-17 12:24:59 -0400

    ghnfg

--- connect 4.py/connect_four_graphical copy.py
+++ connect 4.py/connect_four_graphical copy.py
@@ -1,50 +1,62 @@
+from tkinter import *
+
+root = Tk()
+
+grid_color = "#003AFF"
+red_color = "#FF1800"
+yellow_color = "#FFF600"
+grid_width = 420
+grid_height = grid_width
+diameter = grid_width / 7
+total_rows = 6
+total_cols = 7
+
+screen = Canvas(root, width=grid_width, height=grid_height, bg=grid_color)
+screen.pack()
+
+
 grid = [
-    ["1", "2", "3", "4", "5", "6", "7"],     # [R0C0, R0C1, R0C2]
-    ["-", "-", "-", "-", "-", "-", "-"],     # [R1C0, R1C1, R1C2]
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"]  # [R2C0, R2C1, R2C2]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R0C0, ..., R0C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R1C0, ..., R1C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R2C0, ..., R2C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R3C0, ..., R3C6]
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R4C0, ..., R4C6]
+    ["-", "-", "-", "-", "-", "-", "-"]      # [R5C0, ..., R5C6]
 ]
 
 current_piece = "R"
 
-def print_grid():
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if col != 6:
-                print(grid[row][col], end="  ")            
-            else:
-                print(grid[row][col])
-                print()
+last_row = -1
+last_col = -1
+remaining_spots = 42
 
-def is_bad_num_string(choice : str):
-    if (choice.isnumeric() and int(choice) >= 1 and int(choice) <= 9):
+def draw_circle(row, col, color):
+    x_start = col * diameter
+    y_start = row * diameter
+    x_end = x_start + diameter
+    y_end = y_start + diameter
+    screen.create_oval(x_start, y_start, x_end, y_end, fill=color)
+    pass
+
+
+def draw_grid():
+    for row in range(total_rows):
+        for col in range(total_cols):
+            draw_circle(row, col, "#FFFFFF")  
+
+def is_bad_num_string(choice: str):
+    if (choice.isnumeric() and int(choice) >= 0 and int(choice) <= 6):
         return False
     return True
-                
-def is_bad_choice(choice : str):
-    if(choice.__eq__("STOP")):
+
+
+def is_bad_choice(choice: str):
+    if (choice.__eq__("STOP")):
         return False
     return is_bad_num_string(choice)
 
-def get_row(grid_spot):
-    if grid_spot == 1 or grid_spot == 2 or grid_spot == 3:
-        return 0
-    elif grid_spot == 4 or grid_spot == 5 or grid_spot == 6:
-        return 1
-    else:
-        return 2    
 
-def get_col(grid_spot):
-    if grid_spot == 1 or grid_spot == 4 or grid_spot == 7:
-        return 0
-    elif grid_spot == 2 or grid_spot == 5 or grid_spot == 8:
-        return 1
-    else:
-        return 2
-    
-def place_piece(grid_spot : int):
+def place_piece(col: int):
     global last_row
     global last_col
     global remaining_spots
@@ -56,6 +68,10 @@ def place_piece(grid_spot : int):
                 last_row = row
                 last_col = col
                 remaining_spots -= 1
+                if current_piece.__eq__("R"):
+                    draw_circle(row, col, red_color)
+                else:
+                    draw_circle(row, col, yellow_color)
                 break
             else:
                 row -= 1
@@ -67,6 +83,7 @@ def place_piece(grid_spot : int):
                 user_choice = input(
                     "Enter a different number (0-6) where to drop the piece: ")
             col = int(user_choice)
+
 
 def check_row():
     first_list = [last_col, last_col + 1, last_col + 2, last_col + 3]
@@ -102,18 +119,22 @@ def check_row():
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
     return False
- 
+        
 def check_col():
-    return False
+    return False  # Actually Implement
+
 
 def check_left_diag():
-    return False
+    return False  # Actually Implement
+
 
 def check_right_diag():
-    return False
+    return False  # Actually Implement
 
-def check_draw(): 
+
+def check_draw():
     return remaining_spots == 0
+
 
 def check_game_over():
     if check_row():
@@ -134,23 +155,27 @@ def check_game_over():
     else:
         return False
 
+
 def game_loop():
     global current_piece
-    print("Welcome to TIC TAC TOE")
+    print("Welcome to CONNECT FOUR")
     user_choice = ""
-    while(True):
-        print_grid()
-        while(is_bad_choice(user_choice)):
-            user_choice = input("Enter STOP to end.  Or a number (1-9) where to put the piece: ")
+    draw_grid()
+    while (True):
+        while (is_bad_choice(user_choice)):
+            user_choice = input(
+                "Enter STOP to end.  Or a number (0-6) where to drop the piece: ")
         if user_choice.__eq__("STOP"):
             break
-        grid_spot = int(user_choice)
-        place_piece(grid_spot)
-        if(check_game_over()):
-            print_grid()
+        column = int(user_choice)
+        place_piece(column)
+        if (check_game_over()):
             break
         current_piece = "Y" if current_piece.__eq__("R") else "R"
         user_choice = ""
     print("GAME OVER")
-        
+
+
 game_loop()
+
+mainloop()
